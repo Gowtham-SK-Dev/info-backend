c23bf451c992736d9f0326235454ffa0f28b0b54
Gowtham-SK <81038690+Gowtham-SK-Dev@users.noreply.github.com>
2025-09-29 17:24:52 +0530

    code clean up

--- dist/routes/contact.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contactRoutes = void 0;
-const express_1 = require("express");
-const contactController_1 = require("../controllers/contactController");
-const validation_1 = require("../utils/validation");
-const router = (0, express_1.Router)();
-exports.contactRoutes = router;
-router.post("/contact", validation_1.validateContactForm, contactController_1.contactController);
